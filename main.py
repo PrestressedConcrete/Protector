c110ec18d451ae3f83463cce0882dc4ab06a8225
charlou <charlesazam4@gmail.com>
2024-01-07 18:37:07 +0100

    init

---
+++ main.py
@@ -0,0 +1,60 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+import hashlib
+
+class Protector(App):
+    def build(self):
+        self.layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
+        
+        # Password field
+        self.password = TextInput(password=True, hint_text='Enter password', size_hint=(1, 0.2))
+        self.layout.add_widget(self.password)
+
+        # Input field
+        self.input = TextInput(hint_text='Enter a word', size_hint=(1, 0.2))
+        self.layout.add_widget(self.input)
+
+        # Encode button
+        self.encode_btn = Button(text='Encode', on_press=self.encode, size_hint=(1, 0.2))
+        self.layout.add_widget(self.encode_btn)
+
+        # Output field
+        self.output = TextInput(readonly=True, size_hint=(1, 0.2))
+        self.layout.add_widget(self.output)
+
+        return self.layout
+
+    def _encode(self, password: str, input: str):
+        # Simple encoding using SHA-256 hashing
+        # Concatenate password and input, then hash
+        password = password.split()[0]
+        input = input.split()[0]
+        combined = password + input
+        encoded = hashlib.sha256(combined.encode()).hexdigest()
+
+        if len(input)%3==0:
+            encoded = encoded[-10:]
+            encoded = encoded[3:].upper() + '@' + encoded[3:6] + '#'+ encoded[6:].upper()
+        elif len(input)%3==1:
+            encoded = encoded[-10:]
+            encoded =  '@' + '#'+ encoded[3:] + encoded[3:6].upper() + encoded[6:].upper()
+        else:
+            encoded = encoded[-10:]
+            encoded = encoded[6:] + '@' + encoded[6:7].upper() + '#'+ encoded[7:].upper()
+
+        if encoded.upper() == encoded:
+            encoded = encoded + 'b'
+        if encoded.lower() == encoded:
+            encoded = encoded + 'B'
+        
+        
+        return encoded
+
+    def encode(self, instance):
+        encoded_text = self._encode(self.password.text, self.input.text)
+        self.output.text = encoded_text
+
+if __name__ == '__main__':
+    Protector().run()
